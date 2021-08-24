874b9f407efa11b880f876032a0b24a0c68984d3
Erikson Gonçalves <eriksongoncalves@yahoo.com.br>
2021-08-23 22:45:49 -0300

    Initial Commit

---
+++ imc/ViewController.swift
@@ -0,0 +1,67 @@
+//
+//  ViewController.swift
+//  imc
+//
+//  Created by Erikson Bezerra on 23/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  @IBOutlet weak var tfWeight: UITextField!
+  @IBOutlet weak var tfHeight: UITextField!
+  @IBOutlet weak var lbResult: UILabel!
+  @IBOutlet weak var ivResult: UIImageView!
+  @IBOutlet weak var viResult: UIView!
+  
+  
+  var imc: Double = 0
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+  
+  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    view.endEditing(true)
+  }
+
+  @IBAction func calculate(_ sender: UIButton) {
+    if let weight = Double(tfWeight.text!),
+       let height = Double(tfHeight.text!){
+      imc = weight / (height * height)
+      showResults()
+    }
+    
+    
+  }
+  
+  func showResults(){
+    var result: String = ""
+    var image: String = ""
+    
+    switch imc {
+      case 0..<16:
+        result = "Magreza"
+        image = "magreza"
+      case 16..<18.5:
+        result = "Abaixo do peso"
+        image = "abaixo"
+      case 18.5..<25:
+        result = "Peso ideal"
+        image = "ideal"
+      case 25..<30:
+        result = "Sobrepeso"
+        image = "sobre"
+      default:
+        result = "Obesidade"
+        image = "obesidade"
+    }
+    
+    lbResult.text = result
+    ivResult.image = UIImage(named: image)
+    viResult.isHidden = false
+    view.endEditing(true)
+  }
+}
